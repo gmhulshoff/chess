5f90a682c8f7871ad8546fc88a8afa5c109e6c26
gmhulshoff <gerard@ghulshoff.nl>
2023-08-23 20:50:42 +0200

    Update board.js

--- board.js
+++ board.js
@@ -162,6 +162,6 @@ function removeAllChildren(from, child = from.lastElementChild) {
 }
 
 function setCharAt(str, index, chr) {
-    if(index > str.length-1) return str;
-    return str.substring(0,index) + chr + str.substring(index+1);
+  if(index > str.length-1) return str;
+  return str.substring(0,index) + chr + str.substring(index+1);
 }
